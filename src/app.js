b97c2d9691a67fe9bd2f6eb235fcb2bee322217a
bonnah joohn <bonnahpoku@gmail.com>
2023-01-31 13:05:03 +0000

    changes made in the navBar,the quote and the hero section

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ const eachYear = document.getElementById("date");
 let get = new Date();
 eachYear.innerHTML = get.getFullYear();
 
-console.log(eachYear);
-
 // const btn = document.getElementById("play");
 // btn.addEventListener("click", () => {
 //   document.querySelector("#h").classList.toggle("md:flex");
@@ -12,5 +10,5 @@ console.log(eachYear);
 // });
 
 document.querySelector("button").addEventListener("click", () => {
-  document.getElementById("right").classList.toggle("right-0");
+  document.getElementById("right").classList.toggle("left-0");
 });
