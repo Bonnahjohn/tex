1277c8a931b8a6c1c5138093f5b0e397e8f3a737
bonnah joohn <bonnahpoku@gmail.com>
2023-01-31 06:28:58 +0000

    texas school website is build with tailwind css

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const eachYear = document.getElementById("date");
+
+let get = new Date();
+eachYear.innerHTML = get.getFullYear();
+
+console.log(eachYear);
+
+// const btn = document.getElementById("play");
+// btn.addEventListener("click", () => {
+//   document.querySelector("#h").classList.toggle("md:flex");
+//   document.querySelector("#s").classList.toggle("md:hidden");
+// });
+
+document.querySelector("button").addEventListener("click", () => {
+  document.getElementById("r").classList.toggle("right-0");
+});
